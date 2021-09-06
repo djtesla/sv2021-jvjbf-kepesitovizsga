746c5f74c9a1b11502d810e0abbe3f548cb4b921
djtesla <djtesla@gmailcom>
2021-09-06 12:17:16 +0200

    kezdés commit

---
+++ src/main/java/training360/guinessapp/GuinnesappApplication.java
@@ -0,0 +1,13 @@
+package training360.guinessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuinnesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuinnesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/training360/guinessapp/GuinnesappApplicationTests.java
@@ -0,0 +1,13 @@
+package training360.guinessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuinnesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
